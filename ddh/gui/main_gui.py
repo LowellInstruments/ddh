7836b389aaf2cfabbbf9e1b3a91f64993abe4add
kaz <kaz>
2025-03-20 11:27:02 -0400

    more

--- ddh/gui/main_gui.py
+++ ddh/gui/main_gui.py
@@ -513,9 +513,9 @@ class DDH(QMainWindow, d_m.Ui_MainWindow):
         elif ev.key() == Qt.Key_3:
             lg.a("debug: main_gui detect pressed button 3")
             global g_key3_idx
-            gui_ddh_set_key3_brightness(self, g_key3_idx)
-            # 10 to 19
-            g_key3_idx = 10 + ((g_key3_idx + 1) % 10)
+            # 9 to 18
+            gui_ddh_set_key3_brightness(self, 9 + g_key3_idx)
+            g_key3_idx = (g_key3_idx + 1) % 10
             return
 
         elif ev.key() == Qt.Key_M:
